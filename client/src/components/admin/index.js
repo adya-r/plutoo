562bd0218762e3985b3749d512fea3d1e8e408ad
adya-r <adyaraut123@gmail.com>
2024-11-11 02:26:13 +0530

    first commit

---
+++ client/src/components/admin/index.js
@@ -0,0 +1,6 @@
+import DashboardAdmin from "./dashboardAdmin";
+import Categories from "./categories";
+import Products from "./products";
+
+
+export { DashboardAdmin, Categories, Products, };
---
+++ client/src/components/index.js
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+  Home,
+  WishList,
+  ProtectedRoute,
+  AdminProtectedRoute,
+  CartProtectedRoute,
+  PageNotFound,
+  ProductDetails,
+  ProductByCategory,
+  CheckoutPage,
+} from "./shop";
+import { DashboardAdmin, Categories, Products, } from "./admin";
+import { UserProfile, UserOrders, SettingUser } from "./shop/dashboardUser";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+const Routes = (props) => {
+  return (
+    <Router>
+      <Switch>
+        {}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/wish-list" component={WishList} />
+        <Route exact path="/products/:id" component={ProductDetails} />
+        <Route
+          exact
+          path="/products/category/:catId"
+          component={ProductByCategory}
+        />
+        <CartProtectedRoute
+          exact={true}
+          path="/checkout"
+          component={CheckoutPage}
+        />
+        {}
+
+        {}
+        <AdminProtectedRoute
+          exact={true}
+          path="/admin/dashboard"
+          component={DashboardAdmin}
+        />
+        <AdminProtectedRoute
+          exact={true}
+          path="/admin/dashboard/categories"
+          component={Categories}
+        />
+        <AdminProtectedRoute
+          exact={true}
+          path="/admin/dashboard/products"
+          component={Products}
+        />
+        {}
+
+        {/* User Dashboard */}
+        <ProtectedRoute
+          exact={true}
+          path="/user/profile"
+          component={UserProfile}
+        />
+        <ProtectedRoute
+          exact={true}
+          path="/user/orders"
+          component={UserOrders}
+        />
+        <ProtectedRoute
+          exact={true}
+          path="/user/setting"
+          component={SettingUser}
+        />
+        {/* User Dashboard End */}
+
+        {/* 404 Page */}
+        <Route component={PageNotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
